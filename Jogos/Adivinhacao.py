1dd41e01f7975ed9342244c1f01a875b2ec779e3
Kaique Colognesi <kaique.c.oliveira@hotmail.com>
2020-02-05 20:58:34 -0200

    Initial commit

---
+++ Jogos/Adivinhacao.py
@@ -0,0 +1,30 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+total_de_tetativas = 3
+rodada = 1
+numero_secreto = 42
+
+while(rodada <= total_de_tetativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tetativas))
+
+    chute_str = input("Digite seu numero: ")
+    print("voce digitou: ", chute_str)
+    chute = int(chute_str)
+
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+
+    if (acertou):
+        print("Voce acertou!")
+    else:
+        if(maior):
+            print("Seu chute e maior que o numero secreto")
+        elif(menor):
+            print("Seu chute e menor que o numero secreto")
+
+            rodada +=1
+
+    print("Fim do jogo")
