4a89e3171a96e02939dddba699500bee54b828f5
Kaique Colognesi <kaique.c.oliveira@hotmail.com>
2020-02-05 23:30:37 -0300

    Alterações no laço de repetição
    Alterado laço de repetição para for.
    Numero min, max a ser digitado
    e break para terminar a interação

--- Jogos/Adivinhacao.py
+++ Jogos/Adivinhacao.py
@@ -1,5 +1,9 @@
-#Codigo realizado até a aula 4 do módulo 1 sobre laços de repetição
-#Adicionado variável total_de_tentativas e rodada
+'''
+Alterado o modo que é utilizado para repetição com for,
+adicionado também contador +1 no laço para contablizar 3 tentativas totais.
+Adicionado também validação para o tamanho do numero que deve ser digitado
+E adicionado Break para terminar a interação caso acerte
+'''
 
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
@@ -9,12 +13,17 @@ total_de_tetativas = 3
 rodada = 1
 numero_secreto = 42
 
-while(rodada <= total_de_tetativas):
+for rodada in range(1, total_de_tetativas + 1):
+
     print("Tentativa {} de {}".format(rodada, total_de_tetativas))
 
-    chute_str = input("Digite seu numero: ")
+    chute_str = input("Digite um número de 1 a 100: ")
     print("voce digitou: ", chute_str)
     chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um numero entre 1 e 100!")
+        continue
 
     maior = chute > numero_secreto
     menor = chute < numero_secreto
@@ -22,12 +31,11 @@ while(rodada <= total_de_tetativas):
 
     if (acertou):
         print("Voce acertou!")
+        break
     else:
         if(maior):
             print("Seu chute e maior que o numero secreto")
         elif(menor):
             print("Seu chute e menor que o numero secreto")
-
-            rodada +=1
 
     print("Fim do jogo")
