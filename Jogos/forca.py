6597a505437d8fbaa14c5f6a85e6246234e5e853
Kaique Colognesi <kaique.c.oliveira@hotmail.com>
2020-02-07 15:14:33 -0200

    Inicio do projeto Forca
    Adicionado logica basica para detecção das letras da palavra.
    Palavra ainda é fixa

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -8,6 +8,22 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not acertou and not enforcou):
+
+        chute = input("Qual a letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posicao {}".format(letra, index))
+            index += 1
+
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if (__name__ == "__main__"):
